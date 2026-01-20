dfaa013a9ad88a9cbb73c0eef6cacf31c8a05d64
Rudik Arakelyan <rudik.arakelyan777@gmail.com>
2026-01-19 17:24:30 -0800

    feat: add minimal Phaser scene bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,21 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import Phaser from "phaser";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+class MainScene extends Phaser.Scene {
+  constructor() {
+    super("MainScene");
+  }
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  create() {
+    const text = this.add.text(40, 40, "Shadow Signal", { fontSize: "32px" });
+    text.setShadow(2, 2, "#000", 2);
+  }
+}
+
+new Phaser.Game({
+  type: Phaser.AUTO,
+  parent: "app",
+  width: 960,
+  height: 540,
+  backgroundColor: "#1b1b1b",
+  scene: [MainScene],
+});
